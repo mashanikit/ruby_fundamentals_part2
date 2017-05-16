a89861412a81f2b779a0f225b7b5c8fdfcc6338a
MASHA <mashanikit@gmail.com>
2017-05-16 19:03:02 -0400

    Finished exercise 6

--- exercise0.rb
+++ exercise0.rb
@@ -139,3 +139,39 @@ age.each do |age|
 end
 
 fav_colours.push("red", "coral")
+
+# Make a new hash that contains a list of movies for each year. Each list of movies should be an array. Below is some data you can use.
+#
+# 1999: The Matrix, Star Wars: Episode 1, The Mummy
+# 2009: Avatar, Star Trek, District 9
+# 2019: How to Train Your Dragon 3, Toy Story 4, Star Wars: Episode 9
+# Make a new array that contains each row of the buttons on a phone. Each row should be an array.
+#
+# The rows on a phone are: 1 2 3, 4 5 6, 7 8 9, * 0 #
+# Make a new array that contains information about three countries. Each country should be a hash that has a name, a continent, and whether or not it is an island.
+
+movie_list = {
+  "1999" => ["The Matrix", "Star Wars: Episode 1", "The Mummy"],
+  "2009" => ["Avatar", "Star Trek", "District 9"],
+  "2019" => ["How to Train Your Dragon 3", "Toy Story 4", "Stars Wars: Episode 9"]
+}
+
+phone_buttons = [[1,2,3],
+                [4, 5, 6],
+                [7, 8, 9],
+                ['*', 0, '#']]
+
+country = [
+  {:name => "Canada", :continent => "North America", :island: "Not"},
+  {:name => "United States", :continent => "North America", :island => "Not"},
+  {:name => "Russia", :continent => "Europe", :island => "Not"},
+  {:name => "Australia", :continent => "Australia", :island => "Yes"}
+]
+
+# Output the message "I will not skateboard in the halls" 20 times.
+# Create an array consisting of the above message. It should appear in the array 20 times.
+# Create an array consisting of the numbers between 1 and 50.
+# Use an each loop to find the sum of the numbers in the above array.
+# Create a new array which has three of each number up to 50.
+# Ie. [1, 1, 1, 2, 2, 2, 3, 3, 3, ... , 50, 50, 50] and so on, up to 50.
+# Make a new array out all of the countries from the hash in Exercise 6 that are not islands. Print out both arrays.
